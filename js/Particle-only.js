4abf3ecf6bd90690e817d93fea4d79134ea041d1
ASBEN <serine5627@daum.net>
2022-09-04 21:59:39 +0900

    s

--- js/Particle-only.js
+++
@@ -1,4 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles', './js/A-version-particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded complete');
-});
