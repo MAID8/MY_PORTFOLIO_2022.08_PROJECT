b695e981226febdbb2d7c6b1d12711392f86c15e
ASBEN1 <serine5627@daum.net>
2022-09-06 10:47:49 +0900

    CHANGED UPLOADED 01B

---
+++ PORTFOLIO & Team Works/PT-01/pt01-js/po1-main.js
@@ -0,0 +1,82 @@
+// 헤더 메뉴
+$(function () {
+  $('.a1').click(function () {
+    $('.a1').addClass('ac0'),
+      $('.a1').removeClass('ac1')
+    $('.a2').removeClass('ac2');
+  });
+  $('.a2').click(function () {
+    $('.a1').addClass('ac1'),
+      $('.a2').addClass('ac2'),
+      $('.a1').removeClass('ac0');
+  });
+});
+
+
+// 토글 메뉴 스타일 v1
+// $(function(){
+//   $('.TOI1').click(function(){
+//     $('.TOI1').addClass('TOI1on'),
+//     $('.TOI2').addClass('TOI2on');
+//   });
+//   $('.TOI2').click(function(){
+//     $('.TOI2').removeClass('TOI2on'),
+//     $('.TOI1').removeClass('TOI1on');
+//   });
+
+// });
+
+
+// 토글 메뉴 스타일 v2
+$(function () {
+
+  $('.TOI1').click(function () {
+    $('.TOI1').addClass('TOI1on'),
+      $('.TOI2').addClass('TOI2on'),
+      $('#TGM').addClass('Gon');
+  });
+  $('.TOI2').click(function () {
+    $('.TOI2').removeClass('TOI2on'),
+      $('.TOI1').removeClass('TOI1on'),
+      $('#TGM').removeClass('Gon');
+  });
+
+});
+
+// 검색기능
+// $(function () {
+//   const icon = document.querySelector('.SCicon');
+//   const search = document.querySelector('.searchPart');
+
+//   icon.onclick = (function () {
+//     search.classList.toggle('active');
+//   });
+// });
+
+
+$(function () {
+
+  const width = 690;
+  img_num = 0;
+
+  function playslider(num) {
+    let x = -(width * num);
+
+    $('.slider').css({
+      transform: `translateX(${x}px)`
+    });
+  }
+
+  // 자동재생
+
+  setInterval(function () {
+    img_num++;
+
+    if (img_num >= 3) {
+      img_num = 0;
+    };
+
+    playslider(img_num);
+  }, 7000);
+
+});
---
+++ js/pageload.js
@@ -0,0 +1,27 @@
+// LOADING STYLE VERSION 01
+// $(function(){
+//     $(window).on('load',function(){
+//       $(".loader").fadeOut(1000);
+//       $(".contents").fadeIn(1000);
+//     });
+//   });
+
+
+// LOADING STYLE VERSION 02
+$(function () {
+  $(window).on('load', function () {
+    function counter() {
+      var count = setInterval(function () {
+        var c = parseInt($('.counter').text());
+        $('.counter').text((++c).toString());
+        if (c >= 99) {
+          clearInterval(count);
+          $(".counter").addClass('hide')
+          $(".preloader").addClass('active')
+        }
+        console.log(c);
+      }, 45);
+    }
+    counter();
+  });
+});
